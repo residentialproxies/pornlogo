bae9ff6d1c3fed023c3d7abe28baef77030848d5
Expert Beacon <yonglivelo@gmail.com>
2024-08-01 18:10:18 +0800

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,8 +1,8 @@
 <template>
   <router-link to="/" class="py-12">
     <h1>
-      <span class="text-white p-1">Logoly</span>
-      <span class="text-black bg-custom-primary p-1 rounded-md">Pro</span>
+      <span class="text-white p-1">Porn</span>
+      <span class="text-black bg-custom-primary p-1 rounded-md">Logo</span>
     </h1>
   </router-link>
 </template>
