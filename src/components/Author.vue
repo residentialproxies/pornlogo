c2b8613ab5cd6c1ff2c6b8cb29dff76dbba4e0d0
Expert Beacon <yonglivelo@gmail.com>
2024-08-01 18:09:26 +0800

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,18 +1,10 @@
 <template>
-  <div>
-    <div class="flex flex-col items-center pt-20 pb-12">
-      <img class="max-w-24 rounded-full mb-3" src="../assets/avatar.png" alt="bestony" />
-      <h2 class="text-2xl font-semibold py-2">Bestony</h2>
-      <h4 class="font-semibold">An indie developer / Focus on something interesting.</h4>
-    </div>
+  <div> 
     <div class="flex justify-around items-center py-12">
-      <a class="github" href="https://github.com/bestony"
+      <a class="github" href="https://github.com/expertbeacon/pornlogo"
         ><i class="iconfont icon-github"></i> GitHub</a
-      >
-      <a class="twitter" href="https://twitter.com/xiqingongzi"
-        ><i class="iconfont icon-twitter"></i> Twitter</a
-      >
-      <a class="gmail" href="mailto:xiqingongzi+logoly@gmail.com"
+      > 
+      <a class="gmail" href="mailto:hello@porn-logo.com"
         ><i class="iconfont icon-gmail"></i> Email</a
       >
     </div>
