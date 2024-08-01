2de838ca2e957850c082b3262aba9c11b8eb40a2
Expert Beacon <yonglivelo@gmail.com>
2024-08-01 18:10:34 +0800

    Update Slogan.vue

--- src/components/Slogan.vue
+++ src/components/Slogan.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="mt-12">
     <h3 class="text-xl font-semibold mb-2">
-      <span class="text-white">Logoly</span>
-      <span class="text-black bg-custom-primary p-1 rounded-md">Pro</span>
+      <span class="text-white">Porn</span>
+      <span class="text-black bg-custom-primary p-1 rounded-md">Logo</span>
     </h3>
     <p class="text-white">
-      Logoly.pro is a creative logo generator, you can generate logo similar to Pornhub, YouTube,
-      and more.
-      <strong class="text-custom-primary flex gap-2 mt-2">
-        If you think this project is funny, please
-        <a href="https://github.com/bestony/logoly">
-          <img src="https://img.shields.io/badge/give%20me-a%20star-green.svg" />
-        </a>
-      </strong>
+      Porn-Logo.com is your go-to destination for creating custom logos for your brand. Our free and easy logo generator allows you to design & make your own Pornhub-inspired logo in minutes.
+    </p> 
+  </div>
+
+  <div class="mt-12 text-white"> 
+    <h3 class="text-xl font-semibold mb-2">
+      How to Use Porn-<span class="text-black bg-custom-primary p-1 rounded-md">Hub</span> Logo Maker? 
+    </h3> 
+    <p>
+      Need a Porn Logo? Try Our Free Pornhub Logo Maker! The Porn Logo Maker is very simple to use. You can get a logo in just 4 steps:
     </p>
+    <ul class="list-disc ml-8 my-5">
+      <li><b>Choose Your Style:</b> Select a layout (Pornhub, YouTube, OnlyFans, etc.).</li>
+      <li><b>Enter Your Text: </b>Type your logo text in the center box.</li>
+      <li><b>Customize:</b> Fine-tune colors, background, font, and more.</li>
+      <li><b>Download:</b> Click "Export" and get your custom logo!</li>
+    </ul>
+
+    <h4 class="text-l font-semibold mb-2">Recommended Styles</h4>
+    <ul class="list-disc ml-8 my-5">
+      <li><strong>Pornhub Classic:</strong> The iconic black & orange, bold font design.</li>
+      <li><strong>YouTube Inspired:</strong> Clean, modern, with a play button icon.</li>
+      <li><strong>OnlyFans Chic:</strong> Stylish, contemporary in blue and white.</li>
+      <li><strong>Netflix Vibe:</strong> Sleek, cinematic in red and black.</li>
+      <li><strong>Twitch-Style Fun:</strong> Energetic and eye-catching in purple.</li>
+      <li><strong>Twitter-Esque:</strong> Simple, effective with the bird logo in blue.</li>
+    </ul> 
   </div>
 </template>
