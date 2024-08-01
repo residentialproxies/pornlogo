f90a6b999a7dd614902beb2fad8965a6f603d7e6
Expert Beacon <yonglivelo@gmail.com>
2024-08-01 18:09:37 +0800

    Update Copyright.vue

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -1,6 +1,5 @@
 <template>
   <p class="text-center text-[#666]">
-    © Bestony 2019
-    <a href="https://bitbear.net/?refcode=1ndyezv73" target="_blank">比特熊,微服务</a>
+    © porn-logo.com 2024
   </p>
 </template>
